88e00e70b0ed1abd9e5d93d030ff662fbb02156f
Dougie Lawson <dl1ims@gmail.com>
2021-08-20 07:54:18 +0100

    New JSON key sbm

--- parseJ.c
+++ parseJ.c
@@ -44,7 +44,7 @@ int cts[6];
 int statusCode;
 char* statusText;
 
-enum { unknown, asn, bsm, bss, bst, che, cmt, dat, divi, dom, dow, dst, ectp1, ectp2, ectp3, ectp4, ectt1, ectt2, ectt3, ectt4, ectt5, ectt6, eddi, expd, frq, fwv, gen, gep, grd, h1b, h1d, harvi, hr, imp, lck, mgl, min, mon, nect1, nect2, nect3, pect1, pect2, pect3, pha, pri, pst, pwm, rac, rdc, rrac, sbh, sbk, sno, sta, status, statustext, tbk, tim, tz, v1, vol, yr, zappi, zmo, zs,  zsh, zsl };
+enum { unknown, asn, bsm, bss, bst, che, cmt, dat, divi, dom, dow, dst, ectp1, ectp2, ectp3, ectp4, ectt1, ectt2, ectt3, ectt4, ectt5, ectt6, eddi, expd, frq, fwv, gen, gep, grd, h1b, h1d, harvi, hr, imp, lck, mgl, min, mon, nect1, nect2, nect3, pect1, pect2, pect3, pha, pri, pst, pwm, rac, rdc, rrac, sbh, sbk, sbm, sno, sta, status, statustext, tbk, tim, tz, v1, vol, yr, zappi, zmo, zs,  zsh, zsl };
 
 char* none_str = "None";
 
@@ -131,6 +131,7 @@ int lexer(const char *s)
 		{ "rrac", rrac },
 		{ "sbh", sbh },
 		{ "sbk", sbk },
+		{ "sbm", sbm },
 		{ "sno", sno },
 		{ "sta", sta },
 		{ "status", status },
@@ -323,6 +324,8 @@ json_object* decode_json(json_object* jObj)
 		case sbh:
 			break;
 		case sbk:
+			break;
+		case sbm:
 			break;
 		case sta:
 			break;
