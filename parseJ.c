c224125b265f72f28ddf5aab6762c411881ea5ee
Dougie Lawson <dl1ims@gmail.com>
2023-09-27 17:25:13 +0100

    New JSON key

--- parseJ.c
+++ parseJ.c
@@ -44,7 +44,7 @@ int cts[6];
 int statusCode;
 char* statusText;
 
-enum { unknown, asn, bde, bsm, bss, bst, btw, che, cmt, dat, divi, dom, dow, dst, ectp1, ectp2, ectp3, ectp4, ectp5, ectp6, ectt1, ectt2, ectt3, ectt4, ectt5, ectt6, eddi, expd, frq, fwv, gen, gep, grd, g100, h1b, h1d, harvi, hr, imp, isZ2, lck, mgl, min, mon, nect1, nect2, nect3, napa, nbla, pect1, pect2, pect3, pha, pri, pst, pwm, rac, rdc, rrac, sbh, sbk, sbm, sno, sta, status, statustext, tbk, tim, tz, v1, vhub, vol, yr, zappi, zmo, zs, zsh, zsl };
+enum { unknown, asn, bde, bsm, bss, bst, btw, che, cmt, dat, dcls, divi, dom, dow, dst, ectp1, ectp2, ectp3, ectp4, ectp5, ectp6, ectt1, ectt2, ectt3, ectt4, ectt5, ectt6, eddi, expd, frq, fwv, gen, gep, grd, g100, h1b, h1d, harvi, hr, imp, isZ2, lck, mgl, min, mon, nect1, nect2, nect3, napa, nbla, pect1, pect2, pect3, pha, pri, pst, pwm, rac, rdc, rrac, sbh, sbk, sbm, sno, sta, status, statustext, tbk, tim, tz, v1, vhub, vol, yr, zappi, zmo, zs, zsh, zsl };
 
 char* none_str = "None";
 
@@ -88,6 +88,7 @@ int lexer(const char *s)
 		{ "che", che },
 		{ "cmt", cmt },
 		{ "dat", dat },
+		{ "deviceClass", dcls },
 		{ "div", divi },
 		{ "dom", dom },
 		{ "dow", dow },
@@ -295,6 +296,8 @@ json_object* decode_json(json_object* jObj)
 		case cmt:
 			break;
 		case dat:
+			break;
+		case dcls:
 			break;
 		case divi:
 			break;
